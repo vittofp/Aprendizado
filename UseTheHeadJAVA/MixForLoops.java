79ae232fdccac4dfb6a3588611b40424e09e87c0
Vitor <vitormv725@gmail.com>
2025-10-24 09:09:23 -0300

    Update MixForLoops.java

--- UseTheHeadJAVA/MixForLoops.java
+++ UseTheHeadJAVA/MixForLoops.java
@@ -7,11 +7,8 @@ public class MixForLoops {
         List<Integer> nums = List.of(1, 2, 3, 4, 5);
         String output = "";
 
-        for (int i = 0; i <= nums.size(); i++)
-          output += nums.get(i) + " ";
-
-
-
+       for (Integer num : nums)
+           output += nums + " ";
 
         System.out.println(output);
     }
